37a81922c0aca933cae157c514d184f7b59f3aa0
Bavfalcon9 <olybear9@gmail.com>
2026-03-28 12:28:44 -0500

    more lsp fixes'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,6 +51,11 @@ return {
 				'dockerfile',
 				'lua',
 			})
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function(ev)
+					pcall(vim.treesitter.start, ev.buf)
+				end,
+			})
 		end,
 	},
 	{
